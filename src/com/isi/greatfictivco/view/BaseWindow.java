dc0d6cba8d67752a33d9ff789d638f75d647fb49
mcDazz <notenuf007@hotmail.com>
2015-03-15 14:52:32 -0400

    package view pour BaseWindow

--- src/com/isi/greatfictivco/view/BaseWindow.java
+++ src/com/isi/greatfictivco/view/BaseWindow.java
@@ -1,5 +1,42 @@
 package com.isi.greatfictivco.view;
 
-public class BaseWindow {
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
+@SuppressWarnings("serial")
+public class BaseWindow extends JFrame {
+private Vue vue;
+	
+	public BaseWindow(String title, JMenuBar menuBar){
+		super(title);
+		
+		setSize(1200, 700);
+		setLocation(
+				(int) (java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 2 - 600 ),//600 as constant
+				(int) (java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 2 - (350 )));//350 as constant
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+		this.setJMenuBar(menuBar);
+		
+	}
+	
+	private void ajouterHeaderEtRecherche(ViewController viewController){
+		add(Box.createVerticalStrut(80));
+		add(new Header(viewController));
+		add(Box.createVerticalStrut(20));
+		add(new Search(viewController));
+		add(Box.createVerticalStrut(20));
+	}
+	
+	public void changeVuePrincipale(Vue vue, ViewController viewController){
+		this.vue = vue;
+		getContentPane().removeAll();
+		ajouterHeaderEtRecherche(viewController);
+		add(vue);
+		validate();
+		repaint();
+	}
 }
